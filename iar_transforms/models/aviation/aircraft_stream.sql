4c522dd4cf983c0144a510170add32f41cd38884
Ryan Wyatt <Ryan.Wyatt@inmarsat.com>
2022-04-05 14:35:40 +0100

    improved spacing and usability

--- iar_transforms/models/aviation/aircraft_stream.sql
+++ iar_transforms/models/aviation/aircraft_stream.sql
@@ -2,15 +2,21 @@
     materialized="incremental"
 ) }}
 
+-- incremental load from the cirium_brought_into_service view
 SELECT *
 FROM {{ref('cirium_brought_into_service')}}
+-- code within this block is only executed on an incremental run
 {% if is_incremental() %}
     -- this filter will only be applied on an incremental run
     WHERE CAST(In_Service_Date AS TIMESTAMP) > (SELECT MAX(TS) FROM {{ref('cirium_brought_into_service')}} WHERE ACTIVITY_TYPE = 'brought_into_service')
 {% endif %}
+
 UNION ALL
+
+-- incremental load from the cirium_delivered_to_operator view
 SELECT *
 FROM {{ref('cirium_delivered_to_operator')}}
+-- code within this block is only executed on an incremental run
 {% if is_incremental() %}
     -- this filter will only be applied on an incremental run
     WHERE CAST(Operator_Delivery_Date AS TIMESTAMP) > (SELECT MAX(TS) FROM {{ref('cirium_delivered_to_operator')}} WHERE ACTIVITY_TYPE = 'delivered_to_operator')
